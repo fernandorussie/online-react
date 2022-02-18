3f1a57b4bf3562b238e667c83d20615ff8826e04
Fernando <fernandocesarrussiefelip@gmail.com>
2022-02-18 10:22:23 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import React, { Component }  from 'react'
+import React from 'react';
+export default class Calc extends React.Component {
+  state = {
+    num1: '',
+    num2: '',
+    result: '',
+    sinal: '',
+  };
 
-class Soma extends Component {
-mensagem = (frase, num) => {
-return (
-    <div> 
-    <h1>Meu numero é {num} e meu dobro é {frase}  </h1> 
-   
-    </div>
-)
-  }
-calc = (y) => {
-return y * 60
-}
-  render(){
-    return(
-<div>
-<h1>{this.mensagem(":",50)}</h1>
-<h2>{this.calc(2)}</h2>
+  handleChange = (ev) => {
+    this.setState({
+      num1: Number(ev.target.value),
+    });
+  };
+  handleChange2 = (ev) => {
+    this.setState({
+      num2: Number(ev.target.value),
+    });
+  };
+  sub = () => {
+    let { num1, num2, result } = this.state;
+    // this.setState({
+    //   result: (num1 && num2) !== null ? num1 + num2 : result,
+    //   sinal: '-',
+    // });
+    if (this.state.num1 && this.state.num2 != null) {
+      this.setState({
+        result: this.state.num1 - this.state.num2,
+        sinal: '-',
+      });
+    }else{
+      this.setState({
+        result: "nenhum valor foi digitado"
+      })
+    }
+  };
+  mult = () => {
+    if (this.state.num1 && this.state.num2 != null) {
+      this.setState({
+        result: this.state.num1 * this.state.num2,
+        sinal: 'x',
+      });
+    }else{
+      this.setState({
+        result: "nenhum valor foi digitado"
+      })
+    }
+  };
+  div = () => {
+    let { num1, num2, result } = this.state;
+    if (num1 && num2 != null) {
+      this.setState({
+        result: num1 / num2,
+        sinal: '/',
+      });
+    }else{
+      this.setState({
+        result: "nenhum valor foi digitado"
+      })
+    }
+  };
+  soma = () => {
+    if (this.state.num1 && this.state.num2 != null) {
+      this.setState({
+        result: this.state.num1 + this.state.num2,
+        sinal: '+',
+      });
+    }else{
+      this.setState({
+        result: "nenhum valor foi digitado"
+      })
+    }
+  };
+  media = () => {
+    let {result} = this.state
+    if (this.state.num1 && this.state.num2 != null) {
+      this.setState({
+        result: (this.state.num1 + this.state.num2) / 2,
+        sinal: '+',
+      });
+    }else{
+      this.setState({
+        result: "nenhum valor foi digitado"
+      })
+    }
+  };
+  porcen = () => {
+    if (this.state.num1 && this.state.num2 != null) {
+      this.setState({
+        result: (this.state.num1 + this.state.num2) / 100,
+        sinal: '%',
+      });
+    }else{
+      this.setState({
+        result: "nenhum valor foi digitado"
+      })
+    }
+  };
+  clear = () => {
+    this.setState({
+      num1: '',
+      num2: '',
+      result: '',
+      sinal: '',
+    });
+  };
+  render() {
+    const { sub, mult, div, soma, clear, media, porcen } = this;
+    const { result } = this.state;
+    return (
+      <div>
+        <h1>Calculadora</h1>
+        <input onChange={this.handleChange} value={this.state.num1} />
+        <input onChange={this.handleChange2} value={this.state.num2} />
+        <button onClick={sub}>-</button>
+        <button onClick={mult}>x</button>
+        <button onClick={div}>/</button>
+        <button onClick={soma}>+</button>
+        <button onClick={clear}>C</button>
+        <button onClick={media}>Media</button>
+        <button onClick={porcen}>%</button>
+        <div>
+          <h3>
+            {this.state.num1} {this.state.sinal} {this.state.num2}
+          </h3>
+          <h2>{result}</h2>
+        </div>
       </div>
-    )
+    );
   }
 }
-
-export default Soma 
